66e992928e62ffb5a24ada20238f17dea3d03d73
crooney2016 <32747635+crooney2016@users.noreply.github.com>
2022-05-06 19:46:22 -0500

    good

--- R/script.R
+++ R/script.R
@@ -1,7 +1,7 @@
-var_df <- read.csv(file = "postalzones.csv", header = F)
+var_df <- read.csv("C:\Users\crooney\OneDrive - Century LLC\Desktop\PostalZones\postal\data\postalzones.csv", header = F) # nolint
 for (i in 1:dim(var_df)[1]) {
    this_var_a <- var_df[i,1]
    this_var_b <- var_df[i,2]
-   source([fetch_zones_five_digit()], local=T)
+   source(fetch_zones_five_digit(), local = T)
 
 }
