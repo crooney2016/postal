5704920efdd6a9a7403d3dd6f6efdfadd590b4d6
crooney2016 <32747635+crooney2016@users.noreply.github.com>
2022-05-06 19:35:53 -0500

    commit

--- R/script.R
+++ R/script.R
@@ -1,9 +1,7 @@
 var_df <- read.csv(file = "postalzones.csv", header = F)
-# or read table with correct specs
-for (i in 1:dim(var_df)[1]) { # vectorise for speed; doing it with loops to
+for (i in 1:dim(var_df)[1]) {
    this_var_a <- var_df[i,1]
    this_var_b <- var_df[i,2]
-   source([fetch_zones_five_digit()], local=T) #set local=T as otherwise the vars
-# will not be visible to the script's operations
+   source([fetch_zones_five_digit()], local=T)
 
 }
